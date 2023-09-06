548f0de59de5caa538ebd6465ac32ac260bc3b20
YukiOda <odajpyuki2@gmail.com>
2023-09-06 17:31:03 -0300

    Arquivo realizando testes de validação de dados

---
+++ Testes-01.php
@@ -0,0 +1,25 @@
+<?php
+
+$nome = 'Yuki';
+$sobrenome = 'Fernando';
+$idade = 20;
+$nacionalidade = 'Japão';
+$status_social = 'Solteiro';
+$escolaridade = 'Ensino Médio - Completo';
+$ban = 0
+
+echo $Usuario;
+
+echo "<br>";
+
+if($ban = 1){
+	echo "Acesso Negado - Banido"
+	echo "<br>";
+	echo "Tempo: 24h";
+}elseif($ban != 1){
+	echo "Acesso Permtido"
+	echo "<br>";
+	echo "Seja Bem-Vindo".$usuario;
+}
+
+?>
