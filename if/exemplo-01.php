4efcfd1aeb04d2474008766a4f31c3ed801d0026
YukiOda <odajpyuki2@gmail.com>
2023-09-13 16:50:27 -0300

    somente exemplo01 da aula de if

---
+++ if/exemplo-01.php
@@ -0,0 +1,28 @@
+<?php 
+
+$qualASuaIdade = 20;
+
+$idadeCrianca = 12;
+$idadeMaior = 18;
+$idadeMelhor = 65;
+
+if($qualASuaIdade <= idadeCrianca){
+
+	echo "Criança";
+
+}else if ($qualASuaIdade <= $idadeMaior) {
+	
+	echo "Adolecente";
+
+}else if ($qualASuaIdade <= $idadeMelhor){
+
+	echo "Adulto";
+
+}else{
+
+	echo "Idoso"
+	
+}
+
+
+ ?>
