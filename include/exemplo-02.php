d7d62db32c902c30bfca35174513da69ec3e5536
YukiOda <odajpyuki2@gmail.com>
2023-09-13 16:49:36 -0300

    aula de include

---
+++ include/exemplo-02.php
@@ -0,0 +1,14 @@
+<?php 
+
+var_dump("OK");
+
+//include "inc/exemplo-01.php"; 
+//require "inc/exemplo-01.php";
+require_once "inc/exemplo-01.php";
+
+$resultado = somar(10, 20);
+
+echo $resultado;
+
+
+ ?>
