a2afb6b8012d5c0ca35fd48f41e5d0205f786a1d
YukiOda <odajpyuki2@gmail.com>
2023-09-14 17:09:44 -0300

    acrescentado mais coisas para pratica-01 sobre IF, analisar e verificar possíveis correções

--- if/pratica-01.php
+++ if/pratica-01.php
@@ -4,18 +4,31 @@ $salario = 1500;
 $salarioMinimo = 1200;
 
 if($salario <= $salarioMinimo){
-
 	echo "Você recebe um salario Mínimo de ".$salario;
-
 }else{
-
 	echo "Você recebe mais que um salario Mínimo, recebe ".$salario;
-
 }
 
 echo "<br>";
 
-$diaUtil = 1, 2, 3, 4, 5;
-$quinzenal
+$diaPagamento = 5;
+$bonificação == false;
+
+if ($diaPagamento <= 5) {
+	echo "Recebe nos 5 dias úteis";
+}elseif ($diaPagamento > 5 && $diaPagamento <= 15) {
+	echo "Recebe Quinzenal";
+}elseif ($bonificação === true) {
+	$valorBonificação = 400;
+	echo "Você recebe bonificação dia 20 de".$valorBonificação;
+}else{
+	echo "Não recebe bonificação";
+}
+
+$total = $salario + $valorBonificação
+
+	echo "<br>";
+
+	echo "Valor total de ".$total;
 
 ?>
