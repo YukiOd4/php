12acce5801f2fa05fa9bd855ba2f408c8f22afa0
YukiOda <odajpyuki2@gmail.com>
2023-09-14 13:04:19 -0300

    pratica 1 trata de if sobre salario

---
+++ if/pratica-01.php
@@ -0,0 +1,21 @@
+<?php  
+
+$salario = 1500;
+$salarioMinimo = 1200;
+
+if($salario <= $salarioMinimo){
+
+	echo "Você recebe um salario Mínimo de ".$salario;
+
+}else{
+
+	echo "Você recebe mais que um salario Mínimo, recebe ".$salario;
+
+}
+
+echo "<br>";
+
+$diaUtil = 1, 2, 3, 4, 5;
+$quinzenal
+
+?>
